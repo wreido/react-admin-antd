8479e732e53e4fcb6bc8aeeef3ce68b45ad0131f
ferdinand <983901430@qq.com>
2020-05-11 20:58:58 +0800

    columns抽离

---
+++ src/views/home/columns/listColimns.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+let renderContent = (value, row, index) => {
+  const obj = { children: value, props: {}, }
+  if (index === 4) obj.props.colSpan = 0
+  return obj
+}
+
+export const columns = (vm) => {
+  return [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      render: (text, row, index) => {
+        if (index < 4) return <a href='javescrip:;'>{text}</a>
+        return {
+          children: <a href='javescrip:;'>{text}</a>,
+          props: { colSpan: 5 }
+        }
+      }
+    },
+    { title: 'Age', dataIndex: 'age', render: renderContent },
+    {
+      title: 'Home phone',
+      colSpan: 2,
+      dataIndex: 'tel',
+      render: (value, row, index) => {
+        const obj = { children: value, props: {} }
+        if (index === 2) obj.props.rowSpan = 2
+        if (index === 3) obj.props.rowSpan = 0
+        if (index === 4) obj.props.colSpan = 0
+        return obj
+      }
+    },
+    { title: 'Phone', colSpan: 0, dataIndex: 'phone', render: renderContent },
+    { title: 'Address', dataIndex: 'address', render: renderContent }
+  ]
+}
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import { Button, Table } from 'antd'
 import { setIsLogin } from '@/store/actions/login'
 import $fetch, { $api } from '@/api'
+import { columns } from './columns/listColimns'
 
 @connect(({ login }) => ({
   login
@@ -15,116 +16,15 @@ class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      columns: [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          render: (text, row, index) => {
-            if (index < 4) {
-              return <a href='javescrip:;'>{text}</a>;
-            }
-            return {
-              children: <a href='javescrip:;'>{text}</a>,
-              props: {
-                colSpan: 5,
-              },
-            };
-          },
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-          render: this.renderContent,
-        },
-        {
-          title: 'Home phone',
-          colSpan: 2,
-          dataIndex: 'tel',
-          render: (value, row, index) => {
-            const obj = {
-              children: value,
-              props: {},
-            };
-            if (index === 2) {
-              obj.props.rowSpan = 2;
-            }
-            // These two are merged into above cell
-            if (index === 3) {
-              obj.props.rowSpan = 0;
-            }
-            if (index === 4) {
-              obj.props.colSpan = 0;
-            }
-            return obj;
-          },
-        },
-        {
-          title: 'Phone',
-          colSpan: 0,
-          dataIndex: 'phone',
-          render: this.renderContent,
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          render: this.renderContent,
-        },
-      ],
+      columns: columns(this),
       data: [
-        {
-          key: '1',
-          name: 'John Brown',
-          age: 32,
-          tel: '0571-22098909',
-          phone: 18889898989,
-          address: 'New York No. 1 Lake Park',
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          tel: '0571-22098333',
-          phone: 18889898888,
-          age: 42,
-          address: 'London No. 1 Lake Park',
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          age: 32,
-          tel: '0575-22098909',
-          phone: 18900010002,
-          address: 'Sidney No. 1 Lake Park',
-        },
-        {
-          key: '4',
-          name: 'Jim Red',
-          age: 18,
-          tel: '0575-22098909',
-          phone: 18900010002,
-          address: 'London No. 2 Lake Park',
-        },
-        {
-          key: '5',
-          name: 'Jake White',
-          age: 18,
-          tel: '0575-22098909',
-          phone: 18900010002,
-          address: 'Dublin No. 2 Lake Park',
-        },
+        { key: '1', name: 'John Brown', age: 32, tel: '0571-22098909', phone: 18889898989, address: 'New York No. 1 Lake Park' },
+        { key: '2', name: 'Jim Green', tel: '0571-22098333', phone: 18889898888, age: 42, address: 'London No. 1 Lake Park' },
+        { key: '3', name: 'Joe Black', age: 32, tel: '0575-22098909', phone: 18900010002, address: 'Sidney No. 1 Lake Park' },
+        { key: '4', name: 'Jim Red', age: 18, tel: '0575-22098909', phone: 18900010002, address: 'London No. 2 Lake Park' },
+        { key: '5', name: 'Jake White', age: 18, tel: '0575-22098909', phone: 18900010002, address: 'Dublin No. 2 Lake Park' }
       ]
     }
-  }
-
-
-  renderContent = (value, row, index) => {
-    const obj = {
-      children: value,
-      props: {},
-    };
-    if (index === 4) {
-      obj.props.colSpan = 0;
-    }
-    return obj;
   }
 
   componentDidMount() {
